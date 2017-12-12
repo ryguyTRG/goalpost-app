e4683235e511d64624de2116d543c69c91920650
Ryan Gjoraas <ryangjoraas@Ryans-MacBook-Pro.local>
2017-12-11 22:29:11 -0600

    lesson-03 finished

--- goalpost-app/Controller/CreateGoalVC.swift
+++ goalpost-app/Controller/CreateGoalVC.swift
@@ -40,8 +40,7 @@ class CreateGoalVC: UIViewController, UITextViewDelegate {
         if goalTextView.text != "" && goalTextView.text != "what is your goal?" {
             guard let finishGoalVC = storyboard?.instantiateViewController(withIdentifier: "FinishGoalVC") as? FinishGoalVC else { return }
             finishGoalVC.initData(description: goalTextView.text!, type: goalType)
-            presentDetail(finishGoalVC)
-            
+            presentingViewController?.presentSecondaryDetail(finishGoalVC)
         }
     }
     
--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -9,16 +9,34 @@
 import UIKit
 import CoreData
 
+let appDelegate = UIApplication.shared.delegate as? AppDelegate
+
 class GoalsVC: UIViewController {
     
-    
     @IBOutlet weak var tableView: UITableView!
+    
+    var goals: [Goal] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         tableView.isHidden = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.fetch { (complete) in
+            if complete {
+                if goals.count >= 1 {
+                    tableView.isHidden = false
+                }
+                else {
+                    tableView.isHidden = true
+                }
+            }
+        }
+        tableView.reloadData()
     }
 
     @IBAction func addGoalBtnWasPressed(_ sender: Any) {
@@ -34,15 +52,32 @@ extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return goals.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else { return UITableViewCell() }
-        cell.configureCell(description: "Eat Salad twice a week", type: .shortTerm, goalProgressAmount: 2)
+        let goal = goals[indexPath.row]
+        cell.configureCell(goal: goal)
         return cell
+    }
+}
+
+extension GoalsVC {
+    func fetch(completion: (_ complete: Bool) -> ()) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
         
+        let fetchRequest = NSFetchRequest<Goal>(entityName: "Goal")
         
+        do {
+            goals = try managedContext.fetch(fetchRequest)
+            print("Successfully fetched data.")
+            completion(true)
+        } catch {
+            debugPrint("Could not fetch : \(error.localizedDescription)")
+            completion(false)
+        }
+
     }
 }
 
